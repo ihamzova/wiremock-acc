5e28a66f83f84022628d7ed420905b328ff963a4
Miachev Aleksei <Aleksei.Miachev@t-systems.ru>
2019-07-23 11:42:09 +0300

    DIGIHUB-000

--- src/main/java/com/tsystems/tm/acc/WebhookDefinitionModel.java
+++ src/main/java/com/tsystems/tm/acc/WebhookDefinitionModel.java
@@ -1,26 +1,30 @@
 package com.tsystems.tm.acc;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.http.Body;
 import com.github.tomakehurst.wiremock.http.HttpHeader;
 import com.github.tomakehurst.wiremock.http.RequestMethod;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class WebhookDefinitionModel {
-    private RequestMethod method;
+    private String method;
     private String url;
-    private List<HttpHeader> headers;
+    private Map<String, String> headers;
     private Object jsonBody;
     private Integer fixedDelayMilliseconds;
     private String bodyFileName;
 
     public WebhookDefinitionModel(RequestMethod method, String url, List<HttpHeader> headers, Body body, Integer fixedDelayMilliseconds, String bodyFileName) {
-        this.method = method;
+        this.method = method.getName();
         this.url = url;
-        this.headers = headers;
+        this.headers = new HashMap<>();
+        for (HttpHeader header : headers) {
+            this.headers.put(header.key(), String.join(",", header.values()));
+        }
         try {
             this.jsonBody = body.isBinary() ? null : new ObjectMapper().readValue(body.asString(), Object.class);
         } catch (IOException e) {

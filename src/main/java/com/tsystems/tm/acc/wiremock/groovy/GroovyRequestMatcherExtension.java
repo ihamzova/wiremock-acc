b8c3c485590defabf2a84ef0ce9f81409476d447
Aleksei Miachev <aleksei.miachev@t-systems.com>
2020-09-29 11:38:16 +0200

    DIGIHUB-78521

---
+++ src/main/java/com/tsystems/tm/acc/wiremock/groovy/GroovyRequestMatcherExtension.java
@@ -0,0 +1,54 @@
+package com.tsystems.tm.acc.wiremock.groovy;
+
+import com.github.tomakehurst.wiremock.extension.Parameters;
+import com.github.tomakehurst.wiremock.http.Request;
+import com.github.tomakehurst.wiremock.matching.MatchResult;
+import com.github.tomakehurst.wiremock.matching.RequestMatcherExtension;
+import com.tsystems.tm.acc.wiremock.persist.PersistenceService;
+import groovy.lang.Binding;
+import groovy.lang.GroovyShell;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+
+import static com.github.tomakehurst.wiremock.common.Exceptions.throwUnchecked;
+
+public class GroovyRequestMatcherExtension extends RequestMatcherExtension {
+    private Path filesRoot;
+
+    public GroovyRequestMatcherExtension() {
+        filesRoot = Paths.get(Optional.ofNullable(System.getenv("GROOVY_ROOT")).orElse("./groovy"));
+    }
+
+    @Override
+    public String getName() {
+        return "withGroovy";
+    }
+
+    @Override
+    public MatchResult match(Request request, Parameters parameters) {
+        String inline = parameters.getString("inline");
+        try {
+            Binding b = new Binding();
+            b.setVariable("request", request);
+            b.setVariable("persistence", PersistenceService.get());
+
+            StringBuilder script = new StringBuilder();
+            File builtins = Paths.get(filesRoot.toString(), "builtin.groovy").toFile();
+            if (builtins.exists()) {
+                script.append(FileUtils.readFileToString(builtins));
+                script.append("\n");
+            }
+            script.append(inline);
+            return MatchResult.of((Boolean) new GroovyShell(this.getClass().getClassLoader(), b)
+                    .parse(script.toString())
+                    .run());
+        } catch (Throwable e) {
+            throwUnchecked(e);
+            return MatchResult.noMatch();
+        }
+    }
+}
--- src/main/java/com/tsystems/tm/acc/wiremock/persist/PersistenceHandlebarsHelper.java
+++ src/main/java/com/tsystems/tm/acc/wiremock/persist/PersistenceHandlebarsHelper.java
@@ -2,6 +2,7 @@ package com.tsystems.tm.acc.wiremock.persist;
 
 import com.github.jknack.handlebars.Options;
 import com.github.tomakehurst.wiremock.extension.responsetemplating.helpers.HandlebarsHelper;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.util.Optional;
@@ -22,8 +23,10 @@ public class PersistenceHandlebarsHelper extends HandlebarsHelper<Object> {
 
         if (key.isPresent()) {
             return persistenceService.get(key.get());
+        } else if (!StringUtils.isEmpty(inner)) {
+            return persistenceService.get(inner);
         } else {
-            return inner;
+            return handleError("You must specify key for persistence helper");
         }
     }
 }
